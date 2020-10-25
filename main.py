e7e87b398ccb8646b32690e1e649cc6c57ec64c5
sai <sai.bhushan.2019@gmail.com>
2020-10-25 07:52:41 -0300

    added assets

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 import pygame
-import os
+import assets
+
 
 WIDTH = 360
-HEIGHT = 480
+HEIGHT = 200
 FPS = 30
 
 WHITE = (255, 255, 255)
@@ -16,78 +17,94 @@ pygame.display.set_caption("Sai game")
 clock = pygame.time.Clock()
 
 
-# sprites
-class Player(pygame.sprite.Sprite):
-    y = 0
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((20, 20), )
-        self.image.fill(WHITE)
-        self.rect = self.image.get_rect()
-
-        self.rect.center = (round(WIDTH/2), round(HEIGHT/2))
-
-    def update(self):
-        self.draw_image(DINO)
-
+class DynoAsset(pygame.sprite.Sprite):
+    image = None
     def draw_image(self, string):
         x, y = (0, 0)
         for row in string:
             for character in row:
                 if character == "X":
-                    self.image.set_at((x,y),BLACK)
+                    self.image.set_at((x,y), BLACK)
                 x += 1
             y += 1
             x = 0
 
+# sprites
+class Player(DynoAsset):
+    y = 0
+    def __init__(self):
+        DynoAsset.__init__(self)
+        self.image = pygame.Surface((20, 20), )
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.state = 1
 
-DINO = [
-    #01234567890123456789
-    "____________________",  # 0
-    "___________XXXXXX___",  # 1
-    "__________XXXXXXXX__",  # 2
-    "__________XX_XXXXX__",  # 3
-    "__________XXXXXXXXX_",  # 4
-    "__________XXXX______",  # 5
-    "__________XXXXXXXX__",  # 6
-    "__________XXX_______",  # 7
-    "_________XXXX_______",  # 8
-    "X________XXXXXXXX___",  # 9
-    "X_______XXXXXX__X___",  # 10
-    "XX_____XXXXXXX______",  # 11
-    "_XX___XXXXXXXX______",  # 12
-    "__XXXXXXXXXXX_______",  # 13
-    "____XXXXXXXX________",  # 14
-    "_____XXXXXX_________",  # 15
-    "_____XXX_XX_________",  # 16
-    "_____XX___X_________",  # 17
-    "_____X____X_________",  # 18
-    "_____XX___XX________",  # 19
-    "____________________",  # 20
-]
-CATUS = [
-    "__XX__",
-    "X_XX_X",
-    "XXXXXX",
-    "__XX__",
-    "__XX__",
-    "__XX__",
-]
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
-running = True
+        self.rect.center = (round(WIDTH/2), round(HEIGHT/2))
+        self.frames = {
+            1: assets.CATUS,
+            2: assets.CATUS,
+            0: assets.CATUS,
+            3: assets.CATUS
+        }
 
-while running:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    # Update
-    all_sprites.update()
-    #  Draw / render
-    screen.fill(WHITE)
-    all_sprites.draw(screen)
-    pygame.display.flip()
-pygame.quit()
+    def update(self):
+        dino = self.frames[self.state]
+        self.draw_image(dino)
+
+    def kill_player(self):
+        self.state = 0
+
+class Bird(DynoAsset):
+
+    x = 100
+    def __init__(self):
+        DynoAsset.__init__(self)
+
+        self.state = 1
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (round(WIDTH/2), round(HEIGHT/2))
+
+    def update(self):
+        self.x -= 1
+        string = 1
+        if self.state == 1:
+            string = assets.BIRD_1
+            self.state = 2
+        elif self.state == 2:
+            string = assets.BIRD_2
+            self.state = 1
+
+
+        self.draw_image(string)
+def make_bird(all_sprites):
+    a = random.randint(0 ,50)
+    if a < 3:
+        all_sprites.add(Bird())
+
+def main():
+    all_sprites = pygame.sprite.Group()
+    player = Player()
+    bird = Bird()
+    bird.rect.x = 0
+    bird.rect.y = 0
+    all_sprites.add(player)
+    all_sprites.add(bird)
+    running = True
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # Update
+        all_sprites.update()
+        #  Draw / render
+        screen.fill(WHITE)
+        all_sprites.draw(screen)
+        pygame.display.flip()
+        make_bird(all_sprites)
+
+if __name__ == "__main__":
+    main()
+    pygame.quit()
